91bdc8f59f02e27f32b58d6123f83b954bdf5faf
cesar <haliercesr@gmail.com>
2024-05-06 20:23:42 -0300

    tabla de la verdad

---
+++ src/Operadores.java
@@ -0,0 +1,47 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        System.out.println("HOla, Mundo!");
+
+    
+
+      //OPERADORES: Simbolos que sirven para hacer operaciones con variables o valores
+      //ARITMETICOS:
+
+      int a = 12;
+      int b = a % 2; //el resto nos puede servir por ejemplo para saber si un balor es par o impar
+
+      //ASIGNACIÓN
+      int c=5;
+      c=10;
+      c+=15;
+      c-=5;
+      c*=2;
+      c/=10;
+      c++;
+      c--;
+
+      //OPERADORES DE COMPARACIÓN
+      boolean esMayor= a>b;
+      boolean esMenor= a<b;
+      boolean esIgual= a==b;
+      
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(esMayor);
+      System.out.println(esMenor);
+      System.out.println(esIgual);
+
+      //OPERACIONES DE LÓGICA
+      boolean condicion1=true;
+      boolean condicion2=false;
+
+      boolean resultadoAND=condicion1 && condicion2; //Si las dos son TRUE, el resultado es TRUE
+      boolean resultadoOR=condicion1 || condicion2; //La primera que sea TRUE
+      boolean resultadoNOT=!condicion1;
+
+      System.out.println(resultadoAND);
+      System.out.println(resultadoOR);
+      System.out.println(resultadoNOT);
+
+    }
+}
